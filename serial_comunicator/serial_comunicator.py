1af1761f5ce95f64f15ea48e169418b4eebc5592
renata7zemanova <renata7zemanova@gmail.com>
2016-03-29 17:59:01 +0200

    serial_communicator: added socket parsing whole configuration file, not only first four lines changes comments added some error messages removed debuging outputs

--- serial_comunicator/serial_comunicator.py
+++ serial_comunicator/serial_comunicator.py
@@ -3,7 +3,7 @@
 #nepokracuje, pokud to, co ma byt cislo neni cislo 
 #muze byt pred a za = mezera (i vice)
 #nepokracuje, pokud pred nebo za = neni zadny configuration
-import serial, sys, os
+import serial, sys, os, socket
 
 def escape_sequention(data):
     escape_sequences = {"a" : "\a",
@@ -47,7 +47,7 @@ def escape_sequention(data):
     return data1
 
 def to_number(number, numer_type):
-    print number
+    #print number
     try:
         return numer_type(number)
 
@@ -59,44 +59,44 @@ def configuration_processing():
     configuration = {}#nejak inteligentne pojmenovat
     try:
         with open(os.path.join(os.path.dirname(__file__), 'config.txt')) as f:
-            for i in range(4):
-                cmd1 = f.readline()   
-                if not "=" in cmd1:
+            for line in f:                 
+                if "#" in line:
+                    line = line.split("#")
+                    line = line[0]
+
+                if line == "":
+                    continue
+
+                if not "=" in line:
                     print "v souboru neni ="
                     return -1
 
-                cmd = cmd1.split("=", 1)
-                cmd[0] = cmd[0].strip() #smaze bile znaky na konci a na zacatku radku
-                cmd[-1] = cmd[-1].strip()
+                cmd2 = line.split("=", 1)
+                cmd2[0] = cmd2[0].strip() #smaze bile znaky na konci a na zacatku radku
+                cmd2[0] = cmd2[0].lower()#zmensi vsechny znaky z klice
+                cmd2[-1] = cmd2[-1].strip()
 
-                if cmd[0] == '':
+                if cmd2[0] == '':
                     print "Neni klic do slovniku"
                     return -1
 
-                if cmd[-1] == '':
+                if cmd2[-1] == '':
                     print "Neni, co ulozit do slovniku"
                     return -1
-
-                if "#" in cmd[-1]:
-                    cmd2 = cmd[-1].split("#")
-                    cmd2 = cmd2[0]
-                    cmd[-1] = cmd2
     
-                configuration[cmd[0]] = cmd[-1] 
+                configuration[cmd2[0]] = cmd2[-1] 
     
     except IOError:
         print "nelze otevrit soubor"
         return -1
-       
-    print "configuration processing", configuration
+
     return configuration
 
 def communication(data, configuration): 
-    data = ' '.join(data[1:]) 
-    data1 = escape_sequention(data)
-       
+    data = ' '.join(data[1:])     
+
     if "port" in configuration:
-        cmd = configuration["port"] 
+        port = configuration["port"] 
       
     else:
         print "musis zadat port!"
@@ -116,18 +116,42 @@ def communication(data, configuration):
         wait = configuration["timeout"]  
         wait = to_number(wait, float)
 
-    return v
-    ser = serial.Serial(cmd, v, timeout = wait)#pythonhosted.org/pyserial/pyserial_api.html
-    x = ser.write(data1)
-    ser.close()
-    print 'closed'
+    data1 = escape_sequention(data)
+
+    try:
+        if "address" in configuration:
+            address = configuration["address"]
+            port = to_number(port, int)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((address, port))
+            x = s.send(data1)
+            s.close()
+        
+
+        #return v
+        else:
+            ser = serial.Serial(port, v, timeout = wait)#pythonhosted.org/pyserial/pyserial_api.html
+            x = ser.write(data1)
+            ser.close()
+
+    except serial.serialutil.SerialException as e:
+        print e.message  
+        return -1
+        
+    except socket.error as e:
+        print '{} ({})'.format(e.strerror, e.errno)
+
+        return -1
+    
+
+    print 'closed', x
     return x
 
 if __name__ == '__main__':
-    #data = sys.argv
-    data = str(raw_input('Zadejte data '))
+    data = sys.argv
+    #data = raw_input('Zadejte data: ')
     configuration = configuration_processing() 
     end = communication(data, configuration)  
-    print end
-    os.system("pause")
+    #print end
+    #os.system("pause")
     exit(end)
