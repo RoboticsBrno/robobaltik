592ba21aed16da0b2c6a2c948eaf53e14c66ee7b
Jakub Streit <kuba.streit@gmail.com>
2016-04-01 11:00:14 +0200

    serial_communicator: setup.py requires confirmation with other options than py2exe removed received_data.txt

--- serial_comunicator/setup.py
+++ serial_comunicator/setup.py
@@ -4,6 +4,11 @@ import sys
 
 if len(sys.argv) == 1:
     sys.argv.append('py2exe')
+else:
+    if sys.argv[1] != 'py2exe':
+        print 'Are you sure you realy want {0}?\nThis setup is intended for creating executable by py2exe. I realy do not know what option {0} does.'.format(sys.argv[1])
+        if not raw_input('Continue (y/n)?').lower() in ('y', 'yes'):
+            sys.exit(0)
 
 setup(
     options = {'py2exe': {'bundle_files': 1,
